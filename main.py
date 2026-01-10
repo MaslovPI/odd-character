dad34e6cb6267e9036d3791c01c66b1e8d3a69fb
Pavel Maslov <dnfanl@gmail.com>
2026-01-10 22:42:03 +0100

    Added colors helper for console output

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
+from classes.colors import Colors
 from functions.equipment import generate_equipment
 from functions.roll import roll, rollMultipleAccumulate
 
 
 def main():
+    colors = Colors()
     strength = rollMultipleAccumulate(3, 6)
     dexterity = rollMultipleAccumulate(3, 6)
     willpower = rollMultipleAccumulate(3, 6)
     high = max(strength, dexterity, willpower)
 
     hit_protection = roll(6)
-    print(f"Strength: {strength}")
-    print(f"Dexterity: {dexterity}")
-    print(f"Willpower: {willpower}")
+    print(
+        f"Strength: {colors.colorize(strength, 'red') if strength == high else strength}"
+    )
+    print(
+        f"Dexterity: {colors.colorize(dexterity, 'red') if dexterity == high else dexterity}"
+    )
+    print(
+        f"Willpower: {colors.colorize(willpower, 'red') if willpower == high else willpower}"
+    )
     print(f"Highest Ability Score: {high}")
     print(f"Hit Protection: {hit_protection}")
 
